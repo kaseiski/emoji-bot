a42d6f2fd4c77693c576a091f5b9a02d0a0d329a
Camilla Ett <camilla.ett@gmail.com>
2026-08-01 20:23:53 +0900

    chore: downgrade typescript 7 ->6 & setup CI lint

--- src/misskey/model/ModerationLog.ts
+++ src/misskey/model/ModerationLog.ts
@@ -4,6 +4,7 @@ export type ModerationLog = {
     id: string,
     createdAt: string, // Dateでいけたっけ
     type: string,
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     info: any,
     userId: string, // なんでuserとuseridがあるんですかねぇ
     user: User
